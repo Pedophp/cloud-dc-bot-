e0fa6a6fdca947e48ead3a1cec29baac22b96915
Pedophp <70361440+Pedophp@users.noreply.github.com>
2021-05-06 20:25:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
   DefaultPrefix: "*", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/pdPWxCKKtK", //Support Server Link
-  Token: "ODM2ODMwNzYyNzg2MzU3MjQ5.YIjtYQ.BKEO9ZsDdUAzKtFJH2Aluh3xlXQ", //Discord Bot Token
-  ClientID: "836830762786357249", //Discord Client ID
-  ClientSecret: "xn0sI2CYybFruybNfaw8KVgS9e9eN5Ea", //Discord Client Secret
+  Token: "", //Discord Bot Token
+  ClientID: "", //Discord Client ID
+  ClientSecret: "", //Discord Client Secret
   Scopes: ["identify", "guilds"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   CookieSecret: "Pikachu is cute", //A Secret like a password
@@ -24,7 +24,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: "24ad4499d3b74f3a8269cb72fb344546", //Spotify Client ID
-    ClientSecret: "6826d43240c1442383ec72704c232e5d", //Spotify Client Secret
+    ClientID: "", //Spotify Client ID
+    ClientSecret: "", //Spotify Client Secret
   },
 };
