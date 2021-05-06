8b76149c51d0294036ae396adb4b3d77ee2e3f1d
Pedophp <70361440+Pedophp@users.noreply.github.com>
2021-05-06 20:24:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  Admins: ["462195743646679041"], //Admins of the bot
+  DefaultPrefix: "*", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/pdPWxCKKtK", //Support Server Link
+  Token: "ODM2ODMwNzYyNzg2MzU3MjQ5.YIjtYQ.BKEO9ZsDdUAzKtFJH2Aluh3xlXQ", //Discord Bot Token
+  ClientID: "836830762786357249", //Discord Client ID
+  ClientSecret: "xn0sI2CYybFruybNfaw8KVgS9e9eN5Ea", //Discord Client Secret
+  Scopes: ["identify", "guilds"], //Discord OAuth2 Scopes
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://tenor.com/view/sex-anime-perv-pervert-gif-4841555", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: "http://localhost", //Website where it was hosted at includes http or https
+
+  //Lavalink - Already there is a serer to connect :)
+  Lavalink: {
+    id: "Main",
+    host: "lavalink.sudhanplayz.live",
+    port: 1234,
+    pass: "CodingWithSudhan",
+  },
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: "24ad4499d3b74f3a8269cb72fb344546", //Spotify Client ID
+    ClientSecret: "6826d43240c1442383ec72704c232e5d", //Spotify Client Secret
+  },
+};
